deea647014fc08e0b445e0c5bfebf79608b73cd9
yk <1469617613@qq.com>
2026-03-01 18:49:03 +0800

    更新后端代码 修复登录问题

--- backend/app/utils/__init__.py
+++
@@ -1,1 +0,0 @@
-from .security import verify_password, get_password_hash, create_access_token, create_refresh_token
--- config/api.ts
+++ config/api.ts
@@ -1,5 +1,5 @@
 // 本地调试环境 (Android模拟器用10.0.2.2访问宿主机)
-export const API_BASE_URL = 'http://10.0.2.2:8001';
+export const API_BASE_URL = 'http://127.0.0.1:8001';
 
 // 第三方课表 API
 export const SCHEDULE_API_URL = 'http://101.43.234.149:8003';
